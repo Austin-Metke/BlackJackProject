06fd85985149527bc5480ed50df8ba3dfb08293c
LethalBoar70923 <metke70.am@gmail.com>
2020-11-13 16:28:47 -0800

    Multiplayer Blackjack Test

---
+++ src/Cards.java
@@ -0,0 +1,16 @@
+import javax.swing.*;
+
+class Card {
+
+    ImageIcon imageIcon;
+    String imageName;
+    int value;
+
+    public Card(ImageIcon imageIcon, String imageName) {
+        this.imageName = imageName;
+        this.imageIcon = imageIcon;
+        this.value = value;
+    }
+
+
+}
---
+++ src/Client.java
@@ -0,0 +1,6 @@
+public class Client {
+
+    String ipaddr;
+    int port;
+
+}
